81901facd8056c4120ad08eaeaf1c43ca1aa91ef
rladm <rladm@LAPTOP-I2NUJV7B>
2023-01-18 18:49:34 -0700

    New Project Creation Create a new Web application for HelloWorld.

---
+++ Week02Lab/web/slides.js
@@ -0,0 +1,46 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+window.onload=initLinks;
+
+var myPix = new Array("pics/NewYork.jpg","pics/Italy.jpg","pics/Lavaredo.jpg");
+var thisPic=0;
+
+function initLinks()
+{
+   
+    document.getElementById("prevLink").onclick=processPrevious; 
+    document.getElementById("nextLink").onclick=processNext; 
+}
+
+function processPrevious()
+{
+  
+    if(thisPic==0)
+    {
+      
+        thisPic=myPix.length;
+    }
+   
+    thisPic--;
+    
+    document.getElementById("myPicture").src=myPix[thisPic];
+
+    return false;
+}
+
+function processNext()
+{
+    thisPic++;
+    if(thisPic==myPix.length)
+    {
+        thisPic=0;
+    }
+    document.getElementById("myPicture").src=myPix[thisPic];
+
+    return false;
+}
+
+
