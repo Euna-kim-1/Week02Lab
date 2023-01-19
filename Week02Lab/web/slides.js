f9963985bd63a8ae757c2668f9ccd636da26683e
rladm <rladm@LAPTOP-I2NUJV7B>
2023-01-18 19:01:01 -0700

    New Project Creation Create a new Web application for HelloWorld.

--- Week02Lab/web/slides.js
+++ Week02Lab/web/slides.js
@@ -4,43 +4,34 @@
  * and open the template in the editor.
  */
 window.onload=initLinks;
-
 var myPix = new Array("pics/NewYork.jpg","pics/Italy.jpg","pics/Lavaredo.jpg");
 var thisPic=0;
-
 function initLinks()
-{
-   
-    document.getElementById("prevLink").onclick=processPrevious; 
-    document.getElementById("nextLink").onclick=processNext; 
+{ 
+  document.getElementById("prevLink").onclick=processPrevious; 
+  document.getElementById("nextLink").onclick=processNext; 
 }
 
 function processPrevious()
 {
-  
     if(thisPic==0)
     {
-      
-        thisPic=myPix.length;
+     thisPic=myPix.length;
     }
-   
     thisPic--;
-    
     document.getElementById("myPicture").src=myPix[thisPic];
-
     return false;
 }
 
 function processNext()
 {
-    thisPic++;
-    if(thisPic==myPix.length)
-    {
-        thisPic=0;
-    }
-    document.getElementById("myPicture").src=myPix[thisPic];
-
-    return false;
+  thisPic++;
+  if(thisPic==myPix.length)
+ {
+   thisPic=0;
+  }
+   document.getElementById("myPicture").src=myPix[thisPic];
+   return false;
 }
 
 
